f776a362c05302fe01deea490cdd6a6a3d257d63
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-18 21:52:14 +0530

    chore : Account model created

---
+++ server/models/accountmodel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: "Cash",
+    },
+    balance: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Account", accountSchema);
